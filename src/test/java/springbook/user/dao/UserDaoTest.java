2e26d61fdabe29173b462e8ce39e0caef55630e4
이석준 <leesukjune@wemakeprice.com>
2021-01-23 18:21:18 +0900

    chapter2 > UserDao @Autowired 적용

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,38 +1,32 @@
 package springbook.user.dao;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class UserDaoTest {
 
     @Autowired
     ApplicationContext context;
+    @Autowired
+    UserDao dao;
 
-    private UserDao dao;
     private User user1;
     private User user2;
     private User user3;
 
     @BeforeEach
     void setUp() {
-        dao = context.getBean("userDao", UserDao.class);
         user1 = new User("user1", "이석준1", "111111");
         user2 = new User("user2", "이석준2", "222222");
         user3 = new User("user3", "이석준3", "333333");
