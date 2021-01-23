e3d3e7ebe04489e04601d2ce60765581ff55b1fc
이석준 <leesukjune@wemakeprice.com>
2021-01-23 18:12:12 +0900

    chapter2 > ApplictionContext 를 테스트중 한번만 초기화하도록 변경

---
+++ src/test/java/springbook/user/TobyApplicationTest.java
@@ -0,0 +1,13 @@
+package springbook.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootApplication
+class TobyApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,50 +1,60 @@
 package springbook.user.dao;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setUp() {
+        dao = context.getBean("userDao", UserDao.class);
+        user1 = new User("user1", "이석준1", "111111");
+        user2 = new User("user2", "이석준2", "222222");
+        user3 = new User("user3", "이석준3", "333333");
+    }
 
     @Test
     void addAndGet() throws SQLException {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(DaoFactory.class);
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         assertEquals(0, dao.getCount());
 
-        User user = new User("leesukjune", "이석준", "123123");
-
-        dao.add(user);
+        dao.add(user1);
         assertEquals(1, dao.getCount());
 
-        User user2 = dao.get(user.getId());
+        User user2 = dao.get(user1.getId());
 
-        assertEquals(user.getId(), user2.getId());
-        assertEquals(user.getName(), user2.getName());
-        assertEquals(user.getPassword(), user2.getPassword());
+        assertEquals(user1.getId(), user2.getId());
+        assertEquals(user1.getName(), user2.getName());
+        assertEquals(user1.getPassword(), user2.getPassword());
     }
 
     @Test
     void getCountTest() throws SQLException {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(DaoFactory.class);
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user1 = new User("user1", "이석준1", "111111");
-        User user2 = new User("user2", "이석준2", "222222");
-        User user3 = new User("user3", "이석준3", "333333");
-
         dao.deleteAll();
         assertEquals(0, dao.getCount());
 
@@ -60,10 +70,6 @@ class UserDaoTest {
 
     @Test
     void getUserFailure() throws SQLException {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(DaoFactory.class);
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
 
         assertEquals(0, dao.getCount());
