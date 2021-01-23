865046506edc1212f477922322900002ff414133
이석준 <leesukjune@wemakeprice.com>
2021-01-23 17:33:18 +0900

    chapter2 > 실패 테스트 추가

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -1,32 +1,18 @@
 package springbook.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     String id;
     String name;
     String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -12,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserDaoTest {
 
     @Test
-    void addAndGet() throws SQLException, ClassNotFoundException {
+    void addAndGet() throws SQLException {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(DaoFactory.class);
 
@@ -21,10 +22,7 @@ class UserDaoTest {
         dao.deleteAll();
         assertEquals(0, dao.getCount());
 
-        User user = new User();
-        user.setId("gyumee");
-        user.setName("박성철");
-        user.setPassword("springno1");
+        User user = new User("leesukjune", "이석준", "123123");
 
         dao.add(user);
         assertEquals(1, dao.getCount());
@@ -34,6 +32,43 @@ class UserDaoTest {
         assertEquals(user.getId(), user2.getId());
         assertEquals(user.getName(), user2.getName());
         assertEquals(user.getPassword(), user2.getPassword());
+    }
+
+    @Test
+    void getCountTest() throws SQLException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user1 = new User("user1", "이석준1", "111111");
+        User user2 = new User("user2", "이석준2", "222222");
+        User user3 = new User("user3", "이석준3", "333333");
+
+        dao.deleteAll();
+        assertEquals(0, dao.getCount());
+
+        dao.add(user1);
+        assertEquals(1, dao.getCount());
+
+        dao.add(user2);
+        assertEquals(2, dao.getCount());
+
+        dao.add(user3);
+        assertEquals(3, dao.getCount());
+    }
+
+    @Test
+    void getUserFailure() throws SQLException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+
+        assertEquals(0, dao.getCount());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> dao.get("unknown_id"));
     }
 
 }
