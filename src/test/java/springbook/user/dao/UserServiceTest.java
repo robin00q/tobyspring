c7a71cd22461bfc8c0d86b0c5b7981fc32b04d7a
이석준 <leesukjune@wemakeprice.com>
2021-01-30 17:58:38 +0900

    chapter5 > add 서비스 추가

--- src/test/java/springbook/user/dao/UserServiceTest.java
+++ src/test/java/springbook/user/dao/UserServiceTest.java
@@ -35,6 +35,24 @@ class UserServiceTest {
     }
 
     @Test
+    void add() {
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertEquals(userWithLevel.getLevel(), userWithLevelRead.getLevel());
+        assertEquals(Level.BASIC, userWithoutLevelRead.getLevel());
+    }
+
+    @Test
     void upgradeLevels() {
         userDao.deleteAll();
         for(User user : users) {
