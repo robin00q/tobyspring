1fee0443e9c4074d8ae33035601b8802c852ea79
이석준 <leesukjune@wemakeprice.com>
2021-01-30 16:52:06 +0900

    chapter4 > 기술에 독립적인 UserDao 로 전환

---
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -0,0 +1,52 @@
+
+package springbook.user.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class UserDaoJdbc implements UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<User> userMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        return user;
+    };
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void add(final User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    /**
+     * getForObject 는 하나임이 보장됨
+     */
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                new Object[]{id}, this.userMapper);
+    }
+
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+    }
+}
