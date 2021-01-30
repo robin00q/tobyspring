33185e091e4251935beb8006d0050e9f023c82ad
이석준 <leesukjune@wemakeprice.com>
2021-01-30 18:05:29 +0900

    chapter5 > upgrade 서비스 리팩토링

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -18,4 +18,13 @@ public class User {
     int login;
     int recommend;
 
+    public void upgradeLevel() {
+        Level nextLevel = this.level.nextLevel();
+
+        if(nextLevel == null) {
+            throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+        } else {
+            this.level = nextLevel;
+        }
+    }
 }
