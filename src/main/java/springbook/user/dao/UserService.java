7aa749a0ff64308fb3584bd655117e7f1943eb6f
이석준 <leesukjune@wemakeprice.com>
2021-02-01 09:58:58 +0900

    chapter5 > 메일 전송 기능 추가

--- src/main/java/springbook/user/dao/UserService.java
+++ src/main/java/springbook/user/dao/UserService.java
@@ -6,7 +6,15 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Properties;
 
 public class UserService {
 
@@ -43,6 +51,7 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
     }
 
     private boolean canUpgradeLevel(User user) {
@@ -60,5 +69,26 @@ public class UserService {
             user.setLevel(Level.BASIC);
         }
         userDao.add(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + " 로 업그레이드 되었습니다");
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
