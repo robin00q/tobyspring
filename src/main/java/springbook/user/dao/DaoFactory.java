eb3b91fb8e05cb043cafaccfe65187204afede3a
이석준 <leesukjune@wemakeprice.com>
2021-02-13 12:30:01 +0900

    chapter6 > TransactionAnnotation 을 사용하도록 변경

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,24 +1,15 @@
 package springbook.user.dao;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.mail.MailSender;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
-import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
-import org.springframework.transaction.interceptor.TransactionAttribute;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 import springbook.user.mail.DummyMailSender;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class DaoFactory {
@@ -68,44 +59,37 @@ public class DaoFactory {
     }
 
 //    @Bean
-//    public TransactionAdvice transactionAdvice() {
-//        TransactionAdvice transactionAdvice = new TransactionAdvice();
-//        transactionAdvice.setTransactionManager(transactionManager());
-//        return transactionAdvice;
+//    public TransactionInterceptor transactionAdvice() {
+//        DefaultTransactionAttribute get = new DefaultTransactionAttribute();
+//        get.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+//        get.setReadOnly(true);
+//
+//        DefaultTransactionAttribute all = new DefaultTransactionAttribute();
+//        all.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+//
+//        Map<String, TransactionAttribute> txMap = new HashMap<>(20);
+//        txMap.put("get*", get);
+//        txMap.put("*", all);
+//        NameMatchTransactionAttributeSource nmt = new NameMatchTransactionAttributeSource();
+//        nmt.setNameMap(txMap);
+//
+//        TransactionInterceptor transactionInterceptor = new TransactionInterceptor();
+//        transactionInterceptor.setTransactionManager(transactionManager());
+//        transactionInterceptor.setTransactionAttributeSource(nmt);
+//        return transactionInterceptor;
 //    }
-    @Bean
-    public TransactionInterceptor transactionAdvice() {
-        DefaultTransactionAttribute get = new DefaultTransactionAttribute();
-        get.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        get.setReadOnly(true);
-
-        DefaultTransactionAttribute all = new DefaultTransactionAttribute();
-        all.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-
-        Map<String, TransactionAttribute> txMap = new HashMap<>(20);
-        txMap.put("get*", get);
-        txMap.put("*", all);
-        NameMatchTransactionAttributeSource nmt = new NameMatchTransactionAttributeSource();
-        nmt.setNameMap(txMap);
-
-        TransactionInterceptor transactionInterceptor = new TransactionInterceptor();
-        transactionInterceptor.setTransactionManager(transactionManager());
-        transactionInterceptor.setTransactionAttributeSource(nmt);
-        return transactionInterceptor;
-    }
-
-    @Bean
-    public AspectJExpressionPointcut transactionPointCut() {
-        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-//        pointcut.setExpression("execution(* *..*ServiceImpl.upgrade*(..))");
-        pointcut.setExpression("bean(*Service)");
-        return pointcut;
-    }
-
-    @Bean
-    public DefaultPointcutAdvisor transactionAdvisor() {
-        return new DefaultPointcutAdvisor(transactionPointCut(), transactionAdvice());
-    }
+//
+//    @Bean
+//    public AspectJExpressionPointcut transactionPointCut() {
+//        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+//        pointcut.setExpression("bean(*Service)");
+//        return pointcut;
+//    }
+//
+//    @Bean
+//    public DefaultPointcutAdvisor transactionAdvisor() {
+//        return new DefaultPointcutAdvisor(transactionPointCut(), transactionAdvice());
+//    }
 
     @Bean
     public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
