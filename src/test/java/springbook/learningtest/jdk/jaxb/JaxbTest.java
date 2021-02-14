aa2392eab2dfa55220628a37659a6628b7f5463f
이석준 <leesukjune@wemakeprice.com>
2021-02-14 14:48:57 +0900

    chapter7 > JAXB를 이용한 unmarshalling 을 위한 의존성 추가 및 테스트 추가

---
+++ src/test/java/springbook/learningtest/jdk/jaxb/JaxbTest.java
@@ -0,0 +1,35 @@
+package springbook.learningtest.jdk.jaxb;
+
+import org.junit.jupiter.api.Test;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JaxbTest {
+
+    @Test
+    void readSqlmap() throws JAXBException {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        JAXBContext context = JAXBContext.newInstance(contextPath);
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getClass().getResourceAsStream("/sqlmap.xml"));
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertEquals(3, sqlList.size());
+        assertEquals("add", sqlList.get(0).getKey());
+        assertEquals("insert", sqlList.get(0).getValue());
+        assertEquals("get", sqlList.get(1).getKey());
+        assertEquals("select", sqlList.get(1).getValue());
+        assertEquals("delete", sqlList.get(2).getKey());
+        assertEquals("delete", sqlList.get(2).getValue());
+    }
+}
